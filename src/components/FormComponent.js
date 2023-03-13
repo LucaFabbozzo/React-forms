c87b13fdaa485fef69ab8dd6ea894add804cf651
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-13 17:29:01 +0100

    onchange form

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -13,17 +13,33 @@ export const FormComponent = () => {
       name: data.name.value,
       surname: data.surname.value,
       gender: data.gender.value,
-      bio: data.bio.value
+      bio: data.bio.value,
+      send: data.send.value
     }
     setUser(user);
   }
 
+  const changeData = e => {
+    let input_name = e.target.name;
+    let user_for_change = user;
+
+    //user_for_change[input_name] = e.target.value;
+
+    setUser(prev_state => ({
+        ...prev_state,
+      [input_name]: e.target.value
+     })
+    );
+  
+    console.log(user)
+  }
+  
   return (
     <div>
       <div className="container">
         <h3>React Form</h3>
 
-        {user.bio && user.bio.length >= 1 && (
+        {user.send && (
           <div className="info_user label">
             <p>
               {user.name} {user.surname} es un {user.gender} y su biografie
@@ -34,15 +50,29 @@ export const FormComponent = () => {
         )}
 
         <form onSubmit={getDataFromForms}>
-          <input type="text" placeholder="Name" name="name" />
-          <input type="text" placeholder="Surname" name="surname" />
-          <select name="gender">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            onChange={changeData}
+          />
+          <input
+            type="text"
+            placeholder="Surname"
+            name="surname"
+            onChange={changeData}
+          />
+          <select name="gender" onChange={changeData}>
             <option value="man">Man</option>
             <option value="woman">Woman</option>
           </select>
-          <textarea placeholder="Bio" name="bio"></textarea>
+          <textarea
+            placeholder="Bio"
+            name="bio"
+            onChange={changeData}
+          ></textarea>
 
-          <input type="submit" value="Enviar" />
+          <input type="submit" value="Send" name="send" />
         </form>
       </div>
     </div>
